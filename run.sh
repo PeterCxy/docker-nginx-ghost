2284525529da113c022b6bdde297d1bcd80af2be
Peter Cai <xqsx43cxy@126.com>
2016-01-22 16:14:41 +0800

    run.sh: allow defining non-standard data directory
    to add support for DaoCloud's data volumes
    
    Signed-off-by: Peter Cai <xqsx43cxy@126.com>

--- run.sh
+++ run.sh
@@ -5,6 +5,10 @@ if [[ $GHOST_SITE_HTTPS == true ]]; then
   sed -i "s@#https filtering@sub_filter 'http://${GHOST_SITE_URL}' 'https://${GHOST_SITE_URL}';@g" /etc/nginx/nginx.conf
 fi
 
+if [[ ! -z "$GHOST_SITE_DATA" ]]; then
+  ln -s $GHOST_SITE_DATA /usr/src/ghost/content
+fi
+
 if [[ ! -d /usr/src/ghost/content || ! -d /usr/src/ghost/content/themes ]]; then
   mkdir -p /usr/src/ghost/content
   cp -R /usr/src/ghost/content_default/* /usr/src/ghost/content/
